b7aeddbe96f6faac01fe74e89ec0dee440ef7517
ravis <ravissavoy49@gmail.com>
2021-11-29 11:40:07 -0600

    Added a main

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	//this is output
+	std::cout << "Gib" << std::endl;
+	std::cout << "He likes" "\nBeans.";
+	int number;
+	int number2;
+	std::cin >> number >> number2;
+	return 0;
+}
