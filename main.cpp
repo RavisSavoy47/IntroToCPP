8179ae2e9b56425a5a0655aedacdb9a797b573ee
ravis <ravissavoy49@gmail.com>
2021-11-29 12:53:23 -0600

    Added how to use arrays

--- main.cpp
+++ main.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
 
+int Add(int num1, int num2)
+{
+	return num1 + num2;
+}
+
+int test = 10;
+int numbers[] = { 1, 2, 3 };
+int number2[10] = { 1, 2, 3 };
+
 int main()
 {
 	//this is output
 	std::cout << "Gib" << std::endl;
 	std::cout << "He likes" "\nBeans.";
-	int number;
-	int number2;
-	std::cin >> number >> number2;
+	int age = 12;
+	int health = 10;
+	int damage = 10;
+	int speed = 10;
+	bool isAlive = health;
+
+	for (int i : numbers)
+	{
+		std::cout << numbers[i] << std::endl;
+	}
+
+	std::cin >> age;
+	std::cout << "Your age is: " << age;
 	return 0;
 }
